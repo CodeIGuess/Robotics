441ae8b07921299268778b35bbc114893a56f7fb
CodeIGuess <46802090+CodeIGuess@users.noreply.github.com>
2019-10-04 18:46:10 -0400

    Add files via upload

---
+++ baby_robotB/include/robot-config.h
@@ -0,0 +1,10 @@
+using namespace vex;
+
+vex::brain Brain;
+
+vex::controller Controller1 = vex::controller();
+
+vex::motor front_right     (vex::PORT10, vex::gearSetting::ratio18_1, true);
+vex::motor front_left    (vex::PORT1, vex::gearSetting::ratio18_1, false);
+vex::motor back_right    (vex::PORT20, vex::gearSetting::ratio18_1, true);
+vex::motor back_left    (vex::PORT11, vex::gearSetting::ratio18_1, false);
