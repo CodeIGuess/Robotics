a4f7bcd5a7cbce6cda451dafe46be419db4f4f49
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2019-10-08 16:45:02 -0400

    Revert "Extra `/2`"
    This reverts commit 1dcf63ce9b6cd4cee0da4b3b2178d01822678944.

--- baby_robotB/src/main.cpp
+++ baby_robotB/src/main.cpp
@@ -34,11 +34,11 @@ void usercontrol( void ) {
     float Axis3 = Controller1.Axis3.value();
     float Axis4 = Controller1.Axis4.value();
 
-    front_left.spin(directionType::fwd, (Axis3 + Axis1)/2 - Axis4, velocityUnits::pct);
-    back_left.spin(directionType::fwd, (Axis3 + Axis1)/2 + Axis4, velocityUnits::pct);
+    front_left.spin(directionType::fwd, Axis3 + Axis1 - Axis4, velocityUnits::pct);
+    back_left.spin(directionType::fwd, Axis3 + Axis1 + Axis4, velocityUnits::pct);
 
-    front_right.spin(directionType::fwd, (Axis3 - Axis1)/2 + Axis4, velocityUnits::pct);
-    back_right.spin(directionType::fwd, (Axis3 - Axis1)/2 - Axis4, velocityUnits::pct);
+    front_right.spin(directionType::fwd, Axis3 - Axis1 + Axis4, velocityUnits::pct);
+    back_right.spin(directionType::fwd, Axis3 - Axis1 - Axis4, velocityUnits::pct);
 
     if(Controller1.ButtonA.pressing()){
 
