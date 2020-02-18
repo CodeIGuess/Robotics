c57863c38a64f34c352f92b8135dac727e8076c5
CodeIGuess <46802090+CodeIGuess@users.noreply.github.com>
2020-02-18 16:54:10 -0400

    do
    do not
    
    there is no try

--- pythonReformat.py
+++ pythonReformat.py
@@ -1,9 +1,12 @@
 import sys
-f = list(open("lolxd/main.cpp"))
+#f = list(open("lolxd/src/main.cpp").read())
+f = list("snake_case_test camelCaseTest")
 #f = list(open(sys.argv[0], "r").read())
 
 for a in range(len(f)-1):
-    c = f[a:a+1]
-    print(c)
-    #if c[0] == "_" and c[1].lower() in "qwertyuiopasdfghjklzxcvbnm":
-    #    f[2]
+    if a > len(f):
+        break
+    c = f[a:a+2]
+    if len(c) != 0 and c[0] == "_" and c[1].lower() in "qwertyuiopasdfghjklzxcvbnm":
+        del f[a]
+        f[a] = f[a].upper()
