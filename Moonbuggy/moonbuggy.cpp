8923069b521ec51c63c68dbb21e5d19ca68460cc
jp <56323782+jpVinnie@users.noreply.github.com>
2020-02-19 13:02:29 -0400

    Moonbuggy
    Stuff that isn't VEX

---
+++ Moonbuggy/moonbuggy.cpp
@@ -0,0 +1,40 @@
+const int triggerPin = 12;
+const int echoPin = 11;
+int beeperPin = 13;
+
+void setup() {
+  pinMode(triggerPin, OUTPUT);
+  pinMode(beeperPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  double duration, inches;
+  
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(triggerPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  
+  inches = microsecondsToInches(duration);
+
+  if (inches < 5) {
+    digitalWrite(beeperPin, HIGH);
+  } else {
+    digitalWrite(beeperPin, LOW);
+  }
+  
+  Serial.print(inches);
+  Serial.print("in");
+  Serial.println();
+
+  delay(100);
+}
+
+float microsecondsToInches(float microseconds) {
+  return microseconds / 74 / 2;
+}
